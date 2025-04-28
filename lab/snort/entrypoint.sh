f928a33531fdf815f76b14705fe8424b7b6adb5d
Maesmm <monteil.m.t@gmail.com>
2025-04-28 16:44:54 +0200

    feat(snort): add dockerfile entrypoint

---
+++ lab/snort/entrypoint.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+cd /usr/local/src/libml/examples/classifier
+python3 -m venv venv
+source venv/bin/activate    
+pip install scapy
+python3 pcapgen.py
+
+# Better transfert the trained model
+# pip install numpy tensorflow
+
+/bin/bash
