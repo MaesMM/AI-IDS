c984ee708c464fe27a3117f8000434e158c53886
Arthur LAFONT <arthur.lafont@student.junia.com>
2025-05-02 16:34:34 +0200

    fix(snort):interactive shell with bashrc

--- lab/snort/entrypoint.sh
+++ lab/snort/entrypoint.sh
@@ -1,11 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 cd /usr/local/src/libml/examples/classifier
 python3 -m venv venv
 source venv/bin/activate    
 pip install scapy
 python3 pcapgen.py
 
-# Better transfert the trained model
-# pip install numpy tensorflow ...
-
-/bin/bash
+# Lancer un shell interactif avec chargement du .bashrc
+exec bash --login
